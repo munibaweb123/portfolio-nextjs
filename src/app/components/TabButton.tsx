8f71cf5915cb1b662e096deeceac3383fc5b885a
munibaweb123 <munibavirtualuni@gmail.com>
2024-10-04 00:20:30 +0500

    portfolio

--- src/app/components/TabButton.tsx
+++ src/app/components/TabButton.tsx
@@ -3,7 +3,7 @@ const TabButton =({active, selectTab, children}) =>{
     const buttonClasses = active ? 'text-white border-b border-orange-400':'text-orange-300 border-b border-orange-400'
     return(
         <button onClick={selectTab}>
-            <p className="mr-3 font-semibold hover:text-pink-400 ${buttonClasses}"> 
+            <p className={`mr-3 font-semibold hover:text-pink-400 ${buttonClasses}`}> 
             {children}
             </p>
         </button>
