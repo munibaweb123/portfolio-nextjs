99a9d893b6f138804da7e59aee4d0f237438c5cc
munibaweb123 <munibavirtualuni@gmail.com>
2024-10-06 13:00:02 +0500

    portfolio after some changes

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -4,18 +4,18 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST()) {
+export async function POST() {
     try{
         const data = await resend.emails.send({
             from:'Muniba <shonni.munni@gmail.com>',
             to:['munibavirtualuin@gmail.com'],
             subject:'Hello World',
-            react:(<>
-            <p>Email Body</p></>
+            react:(<div>
+            <p>Email Body</p></div>),
         });
         return NextResponse.json(data);
     }catch (error){
         return NextResponse.json(error)
     };
-    )
+    
 }
